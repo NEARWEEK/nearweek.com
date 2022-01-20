aa808cc6305b915e912ac6b1f7eceeb1870c9638
dzhumanrm <dzhumanrm@gmail.com>
2022-01-20 15:12:21 +0200

    Hide search box on mobile view

--- frontend/src/components/ui/Navbar/Actions/Actions.jsx
+++ frontend/src/components/ui/Navbar/Actions/Actions.jsx
@@ -2,8 +2,10 @@ import * as React from "react";
 import makeStyles from "@mui/styles/makeStyles";
 import Search from "./Search/Search";
 import Connect from "./Connect/Connect";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const Actions = () => {
+  const isMobileMatch = useMediaQuery("(max-width:600px)");
   const useStyles = makeStyles((theme) => ({
     root: {
       display: "flex",
@@ -16,7 +18,7 @@ const Actions = () => {
 
   return (
     <div className={classes.root}>
-      <Search />
+      {!isMobileMatch && <Search />}
       <Connect />
     </div>
   );
