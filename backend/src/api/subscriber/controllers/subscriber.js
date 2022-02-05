4535cca7153fdcb4a9d606f9561ea86cf151c3a8
dzhumanrm <dzhumanrm@gmail.com>
2022-02-05 19:06:25 +0200

    Subscribe and unsubscribe

--- backend/src/api/subscriber/controllers/subscriber.js
+++ backend/src/api/subscriber/controllers/subscriber.js
@@ -1,9 +1,46 @@
-'use strict';
+"use strict";
 
 /**
  *  subscriber controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::subscriber.subscriber');
+module.exports = createCoreController(
+  "api::subscriber.subscriber",
+  ({ strapi }) => ({
+    async subscribe(ctx) {
+      let response;
+      const { email } = ctx.request.body.data;
+      const entity = await strapi.query("api::subscriber.subscriber").findOne({
+        where: { email },
+      });
+
+      if (!entity) {
+        response = await super.create(ctx);
+        return response;
+      }
+
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+    async unsubscribe(ctx) {
+      const { id } = ctx.params;
+      const entity = await strapi.query("api::subscriber.subscriber").findOne({
+        where: { hash: id },
+        populate: {
+          hash: true,
+        },
+      });
+
+      if (entity) {
+        ctx.params.id = entity.id;
+        const response = await super.delete(ctx);
+        return response;
+      }
+      ctx.response.status = 404;
+      return "not implemented";
+    },
+  })
+);
