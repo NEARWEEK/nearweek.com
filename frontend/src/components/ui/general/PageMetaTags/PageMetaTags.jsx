c637e922082e948b66cddf0511cca9b1b1a3b2a0
dzhumanrm <dzhumanrm@gmail.com>
2022-04-15 16:52:33 +0300

    Add tweeter meta tags

--- frontend/src/components/ui/general/PageMetaTags/PageMetaTags.jsx
+++ frontend/src/components/ui/general/PageMetaTags/PageMetaTags.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
 
 const PageMetaTags = ({ title, description, type, url }) => {
+  const setDescription = (desc) => {
+    const el = document.querySelector("meta[property='og:description']");
+    el.setAttribute("content", desc);
+  };
+
+  const setTitle = (title) => {
+    const el = document.querySelector("meta[property='og:title']");
+    el.setAttribute("content", title);
+  };
+
+  const setTweeterTitle = (title) => {
+    const el = document.querySelector("meta[property='tweeter:title']");
+    el.setAttribute("content", title);
+  };
+
+  useEffect(() => {
+    setDescription(description.substring(0, 155));
+  }, [description]);
+
+  useEffect(() => {
+    setTitle(title);
+    setTweeterTitle(title);
+  }, [title]);
+
   return (
-    <Helmet>
-      <meta property="og:description" content={description.substring(0, 155)} />
+    <Helmet prepend>
+      {/*<meta property="og:description" content={description.substring(0, 155)} />*/}
       <meta property="og:type" content="article" />
-      <meta property="og:title" content={title} />
+      {/* <meta property="og:title" content={title} />*/}
       <meta property="og:url" content={url} />
-      <meta property="twitter:title" content={title} />
+      {/* <meta property="twitter:title" content={title} />*/}
       <meta property="twitter:text:title" content={title} />
     </Helmet>
   );
