9618409306ec7d29dbf64a0f7a3cf9c8f12e5494
dzhumanrm <dzhumanrm@gmail.com>
2022-04-15 10:06:11 +0300

    Add image to meta tags

--- frontend/src/components/ui/general/PageMetaTags/PageMetaTags.jsx
+++ frontend/src/components/ui/general/PageMetaTags/PageMetaTags.jsx
@@ -4,19 +4,22 @@ import { Helmet } from "react-helmet";
 const PageMetaTags = ({ title, description, type, url }) => {
   return (
     <Helmet>
+      <meta property="og:description" content={description.substring(0, 155)} />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={title} />
+      <meta property="og:url" content={url} />
       <meta
-        name="description"
-        property="og:description"
-        content={description.substring(0, 155)}
-      />
-      <meta property="og:type" content={type} />
-      <meta name="title" property="og:title" content={title} />
-      <meta name="url" property="og:url" content={url} />
-      <meta
-        name="image"
         property="og:image"
         content={window.location.origin + "/nearweek-logo.png"}
       />
+      <meta
+        property="og:image:secure_url"
+        content={window.location.origin + "/nearweek-logo.png"}
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:title" content={title} />
+      <meta property="og:image:type" content="imge/png" />
     </Helmet>
   );
 };
