ef2240e1b487c895dfd152717507262f9cead667
dzhumanrm <dzhumanrm@gmail.com>
2022-04-18 13:39:41 +0300

    Add meta tags

--- frontend/src/components/ui/general/PageMetaTags/PageMetaTags.jsx
+++ frontend/src/components/ui/general/PageMetaTags/PageMetaTags.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import Markdown from "js-markdown";
 
 const PageMetaTags = ({ title, description, type, url }) => {
-  const html = Markdown.parse(description);
+  const [desc, setDesc] = useState(null);
 
-  const desc = html.replace(/<[^>]+>/g, "");
-
-  console.log(desc);
+  useEffect(() => {
+    const html = Markdown.parse(description);
+    setDesc(html.replace(/<[^>]+>/g, ""));
+  }, [description]);
 
   const setDescription = (desc) => {
     const el = document.querySelector("meta[property='og:description']");
@@ -51,8 +52,12 @@ const PageMetaTags = ({ title, description, type, url }) => {
     <Helmet>
       <meta property="og:type" content="article" />
       <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      {desc && <meta property="og:description" content={desc} />}
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:text:title" content={desc.substring(0, 155)} />
+      {desc && (
+        <meta name="twitter:text:title" content={desc.substring(0, 155)} />
+      )}
       <meta
         name="twitter:image"
         content={`${window.location.origin}/nearweek-logo.jpg`}
