403d19a9dce0155cbad5278e634311cb992a7cbf
dzhumanrm <dzhumanrm@gmail.com>
2022-05-03 17:04:18 +0300

    Modify charts

--- frontend/src/components/ui/general/Stats/StatsBlocks/TotalDistributedBlock.jsx
+++ frontend/src/components/ui/general/Stats/StatsBlocks/TotalDistributedBlock.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 import Block from "./Block/Block";
-import { useChainTransactionStats } from "../../../../../libs/wamp/subscriptions";
 
 const TotalDistributedBlock = () => {
-  const recentTransactionsCount =
-    useChainTransactionStats()?.recentTransactionsCount;
-
-  return (
-    <Block title={"Distributed"} data={recentTransactionsCount} icon={true} />
-  );
+  return <Block title={"Distributed"} data={"-"} icon={true} />;
 };
 
 export default TotalDistributedBlock;
