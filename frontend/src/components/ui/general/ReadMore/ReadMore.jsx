68d3a9590d3ca4724c71975aa91658c3bc282453
dzhumanrm <dzhumanrm@gmail.com>
2022-05-16 20:31:28 +0300

    Fix error

--- frontend/src/components/ui/general/ReadMore/ReadMore.jsx
+++ frontend/src/components/ui/general/ReadMore/ReadMore.jsx
@@ -12,17 +12,18 @@ const ReadMore = ({ children }) => {
   const toggleReadMore = () => {
     setIsReadMore(!isReadMore);
   };
+
   return (
     <Box className={classes.container}>
       <ReactMarkdown className={classes.content}>
-        {isReadMore ? text.slice(0, 230) + "..." : text}
+        {isReadMore ? text.slice(0, 330) + "..." : text}
       </ReactMarkdown>
       <Box
         onClick={toggleReadMore}
         className={classes.readMoreLink}
         sx={{ cursor: "pointer" }}
       >
-        {isReadMore ? "Read more" : "Show less"}
+        {text.length > 330 && isReadMore ? "Read more" : "Show less"}
       </Box>
     </Box>
   );
