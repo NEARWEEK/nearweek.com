9ebbe88bae9d5d91a0ae3b88002437be0753b2ac
rostyslav-dzhuman <89782935+rostyslav-dzhuman@users.noreply.github.com>
2022-05-18 12:13:35 +0300

    TVL chart remove margin

--- frontend/src/components/ui/Stats/TvlChart/TvlChart.jsx
+++ frontend/src/components/ui/Stats/TvlChart/TvlChart.jsx
@@ -175,7 +175,7 @@ const TvlChart = (props) => {
   }
 
   return (
-    <Box className={classes.grid} mt={4}>
+    <Box className={classes.grid}>
       <Paper elevation={0} className={classes.card}>
         <Box p={4}>
           <Typography variant="h5" style={{ fontWeight: 900 }}>
