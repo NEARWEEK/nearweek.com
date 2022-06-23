5e03cb7466244756bf4b9ad29d698fa6414835b1
rostyslav-dzhuman <drm2drm@ukr.net>
2022-06-23 18:18:49 +0300

    Create widget

--- backend/public/js/widget.js
+++ backend/public/js/widget.js
@@ -1,54 +1,36 @@
 var wrgsv = {
-  // идентификатор HTML элемента в который будет размещен виджет
   idBox: "wrgsv",
-  // путь до страницы возвращающей виджет
-  url_wiget: "http://5.161.56.222/api/feeds/widget?populate=*",
-  // путь до страницы стилей виджета
+  url_wiget: "http://5.161.56.222/api/feeds/widget",
   url_style: "http://5.161.56.222/css/widget.css",
-  // метод инициализации виджета
   init: function (id) {
-    // если идентификатор отсутствует, то будем использовать
-    // идентификатор HTML элемента для размещения виджета по умолчанию (т.е. "wrgsv")
     if (!id) {
       id = this.idBox;
     }
     if (document.getElementById(id)) {
-      // подключаем стили виджета
       this.addStyle();
       try {
-        // для кросс-доменного запроса создаем один из ниже указанных объектов
         var XHR =
           "onload" in new XMLHttpRequest() ? XMLHttpRequest : XDomainRequest;
-        // создаем экземпляр объекта
         var xhr = new XHR();
-        // запрос на другой домен (асинхронный)
         xhr.open("GET", this.url_wiget, true);
-        // событие отслеживает, что запрос был успешно завершён
         xhr.onload = function () {
-          // если существует ответ
           if (this.response) {
-            // добавляем полученный ответ в HTML элемент
             document.getElementById(id).innerHTML = this.response;
           }
         };
         xhr.onerror = function () {
           console.log("onerror " + this.status);
         };
-        // отсылаем запрос
         xhr.send();
       } catch (e) {
         console.log(e);
       }
-    }
-    // если на странице не существует HTML элемента с указаным идентификатором
-    // выводим сообщение: блок с идентификатором id="id" отсутствует
-    else {
+    } else {
       console.log('The specified block id="' + id + '" is missing');
     }
   },
-  // метод подключения стилей виджета
   addStyle: function () {
-    style = document.createElement("link");
+    const style = document.createElement("link");
     style.rel = "stylesheet";
     style.type = "text/css";
     style.href = this.url_style;
