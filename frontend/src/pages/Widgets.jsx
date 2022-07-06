e2a98cd7952cd3990164a402b21796d69e7bf50a
rostyslav-dzhuman <drm2drm@ukr.net>
2022-07-06 15:36:58 +0300

    Modify backend api routes

---
+++ frontend/src/pages/Widgets.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/ui/Navbar/Navbar";
+import { Box } from "@mui/material";
+
+const Widgets = () => {
+  return (
+    <>
+      <Navbar />
+      <Box component="main"></Box>
+    </>
+  );
+};
+
+export default Widgets;
